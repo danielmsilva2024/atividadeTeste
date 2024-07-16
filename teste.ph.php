d819c8d64651d5ef089ee3961a05bcfe3cb39ac5
Daniel Morais <165939005+danielmsilva2024@users.noreply.github.com>
2024-07-16 14:41:04 -0300

    Add files via upload

---
+++ teste.ph.php
@@ -0,0 +1,39 @@
+<?php
+
+$valor1=12;
+$valor2=5;
+$valor3=14;
+$valor4=10;
+$valor5=15;
+
+$soma=($valor1+$valor2+$valor3+$valor4);
+
+echo $resultado=($soma/5); 
+
+?>
+
+<?php 
+
+$number1=12;
+$number2=5;
+$number3=14;
+$number4=10;
+
+echo $resultado=($number1/$number2/$number3/$number4)/4;
+
+?> 
+
+<?php 
+$value1=32;
+$value2=5;
+
+echo $resultado=($value1+$value2);
+
+?> 
+
+<?php 
+$number1=12;
+$number2=6;
+$name="Daniel";
+echo $resultado=($number1+$number2). $name;
+?>
